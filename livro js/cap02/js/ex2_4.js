411c6bfdadb2fdbafc3e14cc04cd81b37fbf7da6
Italla Campos <karolynnealencar@hotmail.com>
2025-09-01 21:44:47 -0300

    Exercicio cap 04 - peso

---
+++ livro js/cap02/js/ex2_4.js
@@ -0,0 +1,26 @@
+const frm = document.querySelector("form")
+const resp = document.querySelector("h3")
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const nome = frm.inNome.value
+    const masculino = frm.inMasculino.checked
+    const altura = Number(frm.inAltura.value)
+
+    let peso;
+    if (masculino) {
+        peso = 22 * Math.pow(altura, 2) // Math.pow eleva a altura ao quadrado
+    } else {
+        peso = 21 * Math.pow(altura, 2)
+    } 
+
+    resp.innerText = `${nome}, seu peso ideal é ${peso.toFixed(3)} kg.`
+    
+        
+    })
+
+    frm.addEventListener("reset", () =>{
+        resp.innerText = ""
+        
+    })
