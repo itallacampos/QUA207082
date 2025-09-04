c355731363db4510221c72c4f8bb6517194ade77
Italla Campos <karolynnealencar@hotmail.com>
2025-09-04 19:58:49 -0300

    Exercicio resp4 b.

--- livro js/cap04/resp4_b.js
+++ livro js/cap04/resp4_b.js
@@ -7,6 +7,13 @@ frm.addEventListener("submit", (e) =>{
     const permitida = Number(frm.inPermitida.value)
     const condutor = Number(frm.inCondutor.value)
 
-    if(condutor <= velocidade){}
+    const multa = permitida * 1.20;
 
+    if(condutor <= permitida){
+        resp.innerText = "Situação: Sem multa"
+    }else if(condutor > permitida && condutor <= multa){
+        resp.innerText = "Situação: Multa Leve"
+    }else if(condutor > multa ){
+        resp.innerText = "Situação: Multa Grave"
+    }
 })
