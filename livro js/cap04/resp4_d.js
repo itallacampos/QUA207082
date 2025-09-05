8f134e181299ea637c9c82a234c9b942dcd4c83f
Italla Campos <karolynnealencar@hotmail.com>
2025-09-04 21:44:13 -0300

    Exercicio resp 4 d

---
+++ livro js/cap04/resp4_d.js
@@ -0,0 +1,33 @@
+const frm = document.querySelector("form")
+const resp1 = document.querySelector("#outResp1")
+const resp2 = document.querySelector("#outResp2")
+
+frm.addEventListener("submit", (e) =>{
+    e.preventDefault()
+
+    const ladoa = Number(frm.inLadoA.value)
+    const ladob = Number(frm.inLadoB.value)
+    const ladoc = Number(frm.inLadoC.value)
+
+
+    if(ladoa > ladob + ladoc || ladob > ladoa + ladoc > ladoa + ladob){
+        resp1.innerText = "Não pode ser um triângulo"
+        return        
+    }
+    if(a == b && a == c)  
+        resp1.innerText = "Lados formam um Triângulo"
+        resp2.innerText = "Equilátero"  
+    
+    
+    else if(ladoa == ladob && ladoa != ladoc){
+        resp1.innerText = "Lados formam um Triângulo"
+        resp2.innerText = "Isóceles"
+        
+    }else if(ladoa != ladob && ladoc != ladoa){
+        resp1.innerText = "Lados formam um Triângulo"
+        resp2.innerText = "Escaleno"
+        
+    }
+    
+})
+
