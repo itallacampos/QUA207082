b0cb101edc738567be7311db0d9657eb109dbc49
Italla Campos <karolynnealencar@hotmail.com>
2025-09-05 21:38:34 -0300

    Alterações.

--- livro js/cap04/jogos.js
+++ livro js/cap04/jogos.js
@@ -1,28 +1,24 @@
 const frm = document.querySelector("form")
 const resp = document.querySelector("h3")
-const secreto = Math.round(Math.random()*100)
-let tentativa = 0
+const secreto = Math.round(Math.random()*100) //"fórmula do calcula da variável"
+let tentativa = 0 //let=variavel que muda ao decorrer do projeto
 
 frm.addEventListener("submit", (e) =>{
     e.preventDefault()
 
     const number = Number(frm.inNumero.value)
-    
+    tentativa = tentativa +1
     if(number == secreto ){
-        resp.innerText = `Parabéns, você acertou com ${tentativa} tentativas`
+        resp.innerText = `Parabéns, você acertou com ${tentativa} tentativas` //"${} - usa para inserir variável (colocar crase)"
         return   
 
     }else if(number < secreto ){  
         resp.innerText = "O número é secreto é maior"
-        tentativa = tentativa +1
-        
-       
     }else if(number > secreto){
-        resp.innerText = "O número secreto é menor"
-        tentativa = tentativa +1
-        
+        resp.innerText = "O número secreto é menor"        
     }
 
 })
 
 
+//diferença do else e else if: else if(coloca conteudo nos parametros) - else (não precisa de parâmetro) - obs: Estudar! 
