e201866f1cebb6fa84fcbfa50d136d800b531a85
Italla Campos <karolynnealencar@hotmail.com>
2025-09-12 21:41:55 -0300

    Resolução - correção

--- livro js/cap04/cap05/resp5_c.js
+++ livro js/cap04/cap05/resp5_c.js
@@ -1,21 +1,29 @@
 const frm = document.querySelector("form")
-const resp = document.querySelector("h3")
+const resp1 = document.querySelector("#resp1")
+const resp2 = document.querySelector("#resp2")
+
 
 frm.addEventListener("submit", (e) => {
     e.preventDefault()
 
     const numero = Number(frm.inNumero.value)
-    let soma = 0;
+    let soma = 1;
+    let numDivisores = `Divisores de ${numero}: 1`
 
-    for (let i = 1; i < numero; i++) {
+    for (let i = 2; i <= numero/2; i++) {
         if (numero % i === 0) {
             soma += i;
+            numDivisores += " ," + i
+            // resp1.innerText = `Divisores de ${numero}  ${soma}`  = não ficar repetindo
         }
+
     }
+    resp1.innerText = `Divisores de ${numero}:  ${numDivisores} (Soma: ${soma})`
 
     if (soma === numero) {
-        resp.innerText = `${numero} é um número perfeito`
+        resp2.innerText = `${numero} é um número perfeito`
     } else {
-        resp.innerText = `${numero} não é um número perfeito`
+        resp2.innerText = `${numero} não é um número perfeito`
     }
+    
 })
